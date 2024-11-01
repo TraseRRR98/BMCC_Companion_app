33af19d750edab9ef464cf2406f634a8b5d1999a
TraseRRR98 <traserrr@gmail.com>
2024-11-01 16:16:43 -0400

    Update file uploader to include Bootstrap and jQuery for improved styling and functionality

--- files_uploader/uploader.php
+++ files_uploader/uploader.php
@@ -77,7 +77,10 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Upload Course Files</title>
-    <link href="lib/css.php" rel="stylesheet">
+    <link href="../lib/css.php" rel="stylesheet">
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </head>
 <body>
     <div class="container mt-5">
